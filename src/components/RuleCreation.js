beca4283bd085b7dde3ad9f01b80ad5d4a015a75
gbagolin <giovannibagolin@outlook.it>
2021-04-08 15:07:14 +0200

    Changed buttons name after Send Rule has been clicked.

--- src/components/RuleCreation.js
+++ src/components/RuleCreation.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import edit from "../images/edit.png"
 import add from "../images/plus.png"
 import ModalRuleCreationState from "../states/ModalRuleCreationState"
@@ -10,7 +10,6 @@ import RuleSynthetizedState from "../states/RuleSynthetizedState"
 import ModalRuleEditState from "../states/ModalRuleEditState"
 import RuleSelectedState from "../states/RuleSelectedState"
 import VIEWS from "../util/VIEWS"
-import { Rule } from "postcss"
 import RuleReady from "../states/RuleReady"
 
 export default function RuleCreation() {
@@ -26,6 +25,8 @@ export default function RuleCreation() {
   const isAddRuleDisabled = !(ruleReady.isProblemReady &&
     ruleReady.isTraceReady &&
     ruleReady.isActionReady)
+  const [name, setName] = useState("Send rule")
+  const [animation, setAnimation] = useState("")
   /**
    * returns true if there is a rule for each action
    * and the rule is complete.
@@ -33,12 +34,12 @@ export default function RuleCreation() {
    */
   const isRuleReady = () => {
     for (const action of actions) {
-      if (!(rule.constraints[action] != undefined &&
-        rule.constraints[actionSelected].length > 0)) {
+      if (!(rule.constraints[action.id] != undefined &&
+        rule.constraints[action.id].length > 0)) {
         return false
       }
     }
-    return ruleReady.isRuleReady
+    return (ruleReady.isRuleReady)
   }
   return (
     <div className="border-2 rounded-lg shadow-lg w-96 h-auto m-5 p-5 text-lg">
@@ -51,9 +52,10 @@ export default function RuleCreation() {
             <input className="w-10 h-10 disabled:opacity-50" type="image" src={add} alt="Add ActionSelection"
                    onClick={() => setVisible({ visible: true })}
                    disabled={isAddRuleDisabled} />
-            <button className="ml-5 font-semibold yellow-color rounded-lg p-2 disabled:opacity-50"
-
+            <button className="flex ml-5 font-semibold yellow-color rounded-lg p-2 disabled:opacity-50"
                     onClick={async () => {
+                      setName("Rule Sent")
+                      setAnimation("animation-pulse")
                       const ruleTemplate = []
                       for (let i = 0; i < actions.length; i++) {
                         const variables = new Set()
@@ -76,9 +78,15 @@ export default function RuleCreation() {
                       const response = await axios.post("http://localhost:8001/api/send_rule", ruleTemplate)
                       console.log(response)
                       ruleSynthetized.setRule(response.data)
+                      setName("Rule Sent")
+                      setAnimation("animation-pulse")
                     }}
-                    disabled={isRuleReady()}>Send Rule
+                    disabled={!isRuleReady()}>
+              <div className="animate-pulse">
+                {name}
+              </div>
             </button>
+
           </div>
         </div>
         <div className="m-3"></div>
